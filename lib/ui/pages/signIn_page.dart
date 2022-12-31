cfae99c4dc4666d3831e0cc3f8b52d04a43fadb0
Mhd Dedek Yansyah <dedek2203@gmail.com>
2022-12-31 15:31:39 +0700

    modify signIn,SignUp page with split widget

--- lib/ui/pages/signIn_page.dart
+++ lib/ui/pages/signIn_page.dart
@@ -9,72 +9,93 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return const CustomHeader(
+        title: 'Sign In',
+        subTitle: 'Sign In to Countinue',
+      );
+    }
+
+    Widget emailInput() {
+      return CustomFormField(
+        label: 'Email Address',
+        hintText: 'Your Email Address',
+        controller: emailController,
+        assetIcon: 'assets/icons/ic_email.svg',
+      );
+    }
+
+    Widget passwordInput() {
+      return CustomFormField(
+        label: 'Password',
+        hintText: 'Your Password',
+        controller: passwordController,
+        assetIcon: 'assets/icons/ic_password.svg',
+      );
+    }
+
+    Widget signInButton() {
+      return CustomButton(
+        onPressed: () => Navigator.pushNamed(context, '/main-home'),
+        label: 'Sign In',
+        height: 6.h,
+      );
+    }
+
+    Widget footer() {
+      return Container(
+        margin: const EdgeInsets.only(bottom: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Don\'t have an account?',
+              style: grayStyle.copyWith(fontSize: 12.sp),
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            GestureDetector(
+              onTap: () => Navigator.pushNamed(context, '/sign-up'),
+              child: Text(
+                'Sign Up',
+                style: primaryStyle.copyWith(fontSize: 12.sp),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-        backgroundColor: defaultBackground,
-        body: SafeArea(
-          child: Container(
-            width: 100.w,
-            height: 100.h,
-            padding: EdgeInsets.all(defaultPadding),
-            child: Stack(children: [
-              SingleChildScrollView(
-                  child: Column(
-                children: [
-                  const CustomHeader(
-                    title: 'Sign In',
-                    subTitle: 'Sign In to Countinue',
-                  ),
-                  CustomFormField(
-                    label: 'Email Address',
-                    hintText: 'Your Email Address',
-                    controller: emailController,
-                    assetIcon: 'assets/icons/ic_email.svg',
-                  ),
-                  SizedBox(
-                    height: defaultMargin,
-                  ),
-                  CustomFormField(
-                    label: 'Password',
-                    hintText: 'Your Password',
-                    controller: passwordController,
-                    assetIcon: 'assets/icons/ic_password.svg',
-                  ),
-                  SizedBox(
-                    height: defaultMargin,
-                  ),
-                  CustomButton(
-                    onPressed: () => Navigator.pushNamed(context, '/main-home'),
-                    label: 'Sign In',
-                    height: 6.h,
-                  ),
-                ],
-              )),
-              Positioned(
-                left: 0,
-                bottom: 0,
-                right: 0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                      style: grayStyle.copyWith(fontSize: 12.sp),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    GestureDetector(
-                      onTap: () => Navigator.pushNamed(context, '/sign-up'),
-                      child: Text(
-                        'Sign Up',
-                        style: primaryStyle.copyWith(fontSize: 12.sp),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ]),
+      backgroundColor: defaultBackground,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          width: 100.w,
+          height: 100.h,
+          padding: EdgeInsets.all(defaultPadding),
+          child: Column(
+            children: [
+              header(),
+              SizedBox(
+                height: defaultMargin * 2,
+              ),
+              emailInput(),
+              SizedBox(
+                height: defaultMargin,
+              ),
+              passwordInput(),
+              SizedBox(
+                height: defaultMargin,
+              ),
+              signInButton(),
+              const Spacer(),
+              footer()
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/ui/pages/signUp_page.dart
+++ lib/ui/pages/signUp_page.dart
@@ -11,91 +11,116 @@ class SignUp extends StatelessWidget {
       TextEditingController(text: '');
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: defaultBackground,
-        body: SafeArea(
-          child: Container(
-            color: defaultBackground,
-            width: 100.w,
-            height: 100.h,
-            padding: EdgeInsets.all(defaultPadding),
-            child: Stack(children: [
-              SingleChildScrollView(
-                  child: Column(
-                children: [
-                  const CustomHeader(
-                    title: 'Sign Up',
-                    subTitle: 'Register and Happy Shoping',
-                  ),
-                  CustomFormField(
-                    label: 'Full Name',
-                    hintText: 'Your Full Name',
-                    controller: nameController,
-                    assetIcon: 'assets/icons/ic_fullname.svg',
-                  ),
-                  SizedBox(
-                    height: defaultMargin,
-                  ),
-                  CustomFormField(
-                    label: 'Username',
-                    hintText: 'Your Username',
-                    controller: passwordController,
-                    assetIcon: 'assets/icons/ic_username.svg',
-                  ),
-                  SizedBox(
-                    height: defaultMargin,
-                  ),
-                  CustomFormField(
-                    label: 'Email Address',
-                    hintText: 'Your Email Address',
-                    controller: emailController,
-                    assetIcon: 'assets/icons/ic_email.svg',
-                  ),
-                  SizedBox(
-                    height: defaultMargin,
-                  ),
-                  CustomFormField(
-                    label: 'Password',
-                    hintText: 'Your Password',
-                    controller: passwordController,
-                    assetIcon: 'assets/icons/ic_password.svg',
-                  ),
-                  SizedBox(
-                    height: defaultMargin,
-                  ),
-                  CustomButton(
-                    onPressed: () {},
-                    label: 'Sign Up',
-                    height: 6.h,
-                  ),
-                ],
-              )),
-              Positioned(
-                left: 0,
-                bottom: 0,
-                right: 0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                      style: grayStyle.copyWith(fontSize: 12.sp),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    GestureDetector(
-                      onTap: () => Navigator.pop(context),
-                      child: Text(
-                        'Sign In',
-                        style: primaryStyle.copyWith(fontSize: 12.sp),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ]),
+    Widget header() {
+      return const CustomHeader(
+        title: 'Sign Up',
+        subTitle: 'Register and Happy Shoping',
+      );
+    }
+
+    Widget fullnameInput() {
+      return CustomFormField(
+        label: 'Full Name',
+        hintText: 'Your Full Name',
+        controller: nameController,
+        assetIcon: 'assets/icons/ic_fullname.svg',
+      );
+    }
+
+    Widget usernameInput() {
+      return CustomFormField(
+        label: 'Username',
+        hintText: 'Your Username',
+        controller: passwordController,
+        assetIcon: 'assets/icons/ic_username.svg',
+      );
+    }
+
+    Widget emailInput() {
+      return CustomFormField(
+        label: 'Email Address',
+        hintText: 'Your Email Address',
+        controller: emailController,
+        assetIcon: 'assets/icons/ic_email.svg',
+      );
+    }
+
+    Widget passwordInput() {
+      return CustomFormField(
+        label: 'Password',
+        hintText: 'Your Password',
+        controller: passwordController,
+        assetIcon: 'assets/icons/ic_password.svg',
+      );
+    }
+
+    Widget signUpButton() {
+      return CustomButton(
+        onPressed: () {},
+        label: 'Sign Up',
+        height: 6.h,
+      );
+    }
+
+    Widget footer() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Don\'t have an account?',
+            style: grayStyle.copyWith(fontSize: 12.sp),
           ),
-        ));
+          const SizedBox(
+            width: 5,
+          ),
+          GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Text(
+              'Sign In',
+              style: primaryStyle.copyWith(fontSize: 12.sp),
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: defaultBackground,
+      body: SafeArea(
+        child: Container(
+          color: defaultBackground,
+          width: 100.w,
+          height: 100.h,
+          padding: EdgeInsets.all(defaultPadding),
+          child: Column(
+            children: [
+              header(),
+              SizedBox(
+                height: defaultMargin * 2,
+              ),
+              fullnameInput(),
+              SizedBox(
+                height: defaultMargin,
+              ),
+              usernameInput(),
+              SizedBox(
+                height: defaultMargin,
+              ),
+              emailInput(),
+              SizedBox(
+                height: defaultMargin,
+              ),
+              passwordInput(),
+              SizedBox(
+                height: defaultMargin,
+              ),
+              signUpButton(),
+              const Spacer(),
+              footer()
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
